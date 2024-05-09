df3a4034bfd82bf288f4c7aa094e263965d6c138
dan <md.erhenede@gmail.com>
2024-05-09 11:04:47 +0100

    pp

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Images } from './Images'
 
-import car1 from './assets/car game/car card-1.png' 
-import car2 from './assets/car game/car card.png' 
-import car3 from './assets/car game/gallery-1.png' 
-import car4 from './assets/car game/gallery.png' 
-
-import shopify1 from './assets/shopify/gallary-1.png' 
-import shopify2 from './assets/shopify/gallary.png' 
-import shopify3 from './assets/shopify/shopify card.png' 
-import shopify4 from './assets/shopify/shopify long.png' 
 
 function App() {
 
-  const [car, setCar] = useState(false)
-  const [shopify, setShopify] = useState(false)
 
   return (
     <>
@@ -26,31 +15,12 @@ function App() {
       </div>
       <h1>portfolio gallery</h1>
 
-     {/* car game */}
+     {/* data */}
       <div>
-        <button onClick={()=>setCar(!car)}>Car Game</button>
-        {car &&  
-          <div>
-            <img src={car1} className='image' alt="" />
-            <img src={car2} className='image' alt="" />
-            <img src={car3} className='image' alt="" />
-            <img src={car4} className='image' alt="" />
-          </div>
-          }
+        {Images.map(image=><img src={image} className='image' alt="" />)}
+            
       </div>
 
-     {/* car game */}
-      <div>
-        <button onClick={()=>setShopify(!shopify)}>shopify</button>
-        {shopify &&  
-          <div>
-            <img src={shopify1} className='image' alt="" />
-            <img src={shopify2} className='image' alt="" />
-            <img src={shopify3} className='image' alt="" />
-            <img src={shopify4} className='image' alt="" />
-          </div>
-          }
-      </div>
 
     </>
   )
---
+++ src/Images.tsx
@@ -0,0 +1,48 @@
+import car1 from './assets/car game/car card-1.png' 
+import car2 from './assets/car game/car card.png' 
+import car3 from './assets/car game/gallery-1.png' 
+import car4 from './assets/car game/gallery.png' 
+
+import shopify1 from './assets/shopify/gallary-1.png' 
+import shopify2 from './assets/shopify/gallary.png' 
+import shopify3 from './assets/shopify/shopify card.png' 
+import shopify4 from './assets/shopify/shopify long.png' 
+
+import movie1 from './assets/movie/gallery-1.png' 
+import movie2 from './assets/movie/gallery.png' 
+import movie3 from './assets/movie/movie card-1.png' 
+import movie4 from './assets/movie/movie card.png' 
+
+import pokedex1 from './assets/pokedex/gallery.png' 
+import pokedex2 from './assets/pokedex/pokedex card-1.png' 
+import pokedex3 from './assets/pokedex/pokedex card.png' 
+
+import sizzles1 from './assets/sizzles/page 1.png' 
+import sizzles2 from './assets/sizzles/page 13.png' 
+import sizzles3 from './assets/sizzles/page 14.png' 
+import sizzles4 from './assets/sizzles/page 2.png' 
+import sizzles5 from './assets/sizzles/page 3.png' 
+import sizzles6 from './assets/sizzles/page 4.png' 
+import sizzles7 from './assets/sizzles/page 5.png' 
+import sizzles8 from './assets/sizzles/page 6.png' 
+
+import trade1 from './assets/trade/car card-1.png' 
+import trade2 from './assets/trade/car card.png'
+import trade3 from './assets/trade/gallery-1.png'
+import trade4 from './assets/trade/gallery.png'
+
+import youtube1 from './assets/youtube/gallery.png'
+import youtube2 from './assets/youtube/movie card-1.png'
+import youtube3 from './assets/youtube/movie card.png'
+
+
+export const Images = [
+
+    car1,car2,car3,car4,
+    shopify1,shopify2,shopify3,shopify4,
+    movie1,movie2,movie3,movie4,
+    pokedex1,pokedex2,pokedex3,
+    sizzles1,sizzles2,sizzles3,sizzles4,sizzles5,sizzles6,sizzles7,sizzles8,
+    trade1,trade2,trade3,trade4,
+    youtube1,youtube2,youtube3,
+]
